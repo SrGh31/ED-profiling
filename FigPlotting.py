56f50696545e04f8b33cbf4951bbb99ae0ef2fe1
Sreejita Ghosh <ghosh.sreejita@gmail.com>
2024-11-04 18:22:13 +0100

    Added new jupyter notebook with PPCA, new dataset discussed with Pia, and cluster to cluster distances as clustermaps along with the cluster profiles for Mladena and Pia to check

--- FigPlotting.py
+++ FigPlotting.py
@@ -7,6 +7,7 @@ import warnings
 warnings.filterwarnings('ignore')
 import time, itertools, re
 import matplotlib.patches as patches
+from scipy.cluster.hierarchy import dendrogram
 
 def clusterPlot(cluster_model, data_tabs, fig_naming, fig_lab_titles):
     fig, ax=plt.subplots(1,2, figsize=(8,4), layout='constrained')
@@ -54,3 +55,27 @@ def clusterPlot(cluster_model, data_tabs, fig_naming, fig_lab_titles):
     plt.savefig('figs/PNGs/ED_%s_%s.png'%(datasettag, saveExpName))
     zs_ed_cluster_df=pd.DataFrame.from_dict(zs_cluster_median_profile).T
     return zs_ed_cluster_df, ed_cluster_df
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+    
